31d5836be8278d3b2202ff4a832ce38c05087ab0
yasudamakoto <hare.v1v1enne@gmail.com>
2021-12-08 01:50:52 +0900

    form作成

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // textViewのtextにmessageを代入する
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+    }
+}
--- MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.myfirstapp
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
 
 // アプリのエントリーポイント
 class MainActivity : AppCompatActivity() {
+
+    // activity_mainからUIを作成
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    // 送信ボタンのイベント
+    // hogehoge(引数: 型)
+    fun sendMessage(view : View) {
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+
+        // editTextの文字列をIntentに追加する
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        // Intentを使用して、Activity間でデータを渡すことができる
+        startActivity(intent)
+    }
 }
+
+
+// memo
+// ktファイルを作成すると、xmlファイルが生成されて、
+// activityがAndroidManifest.xmlに追加
+
+// AndroidManifest.xmlファイル
+// <activity android:name=".DisplayMessageActivity" android:parentActivityName=".MainActivity">
+// 上記はandroid:nameが子Activityで、android:parentActivityNameが親Activity
