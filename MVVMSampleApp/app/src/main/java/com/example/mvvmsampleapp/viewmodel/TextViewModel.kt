7eb7cd80e03003641359d7218595836c6711bc49
yasudamakoto <hare.v1v1enne@gmail.com>
2021-12-24 02:14:26 +0900

    モデル追加

--- MVVMSampleApp/app/src/main/java/com/example/mvvmsampleapp/viewmodel/TextViewModel.kt
+++ MVVMSampleApp/app/src/main/java/com/example/mvvmsampleapp/viewmodel/TextViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.mvvmsampleapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.mvvmsampleapp.model.DataModel
 
 class TextViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    var liveDataText: MutableLiveData<String> = MutableLiveData()
+
+    fun fetchText() {
+        liveDataText.value = DataModel().fetchText()
+    }
 }
