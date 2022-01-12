ecc99a2b3cb1d4b1546f04a4fe50d5b61a6ebba9
yasudamakoto <hare.v1v1enne@gmail.com>
2022-01-13 00:10:55 +0900

    一部修正

--- TodoApp/app/src/main/java/com/example/todoapp/model/TodoItem.kt
+++ TodoApp/app/src/main/java/com/example/todoapp/model/TodoItem.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class TodoItem (
     @PrimaryKey(autoGenerate = true)
-    val id: Int,
+    val _id: Int = 0,
     val title: String,
     val description: String,
     val isCompleted: Boolean
--- TodoApp/app/src/main/java/com/example/todoapp/repository/ToDoItemRepositoryImpl.kt
+++ TodoApp/app/src/main/java/com/example/todoapp/repository/ToDoItemRepositoryImpl.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.withContext
 class ToDoItemRepositoryImpl (private val dao: TodoItemDAO): ToDoItemRepository {
     override suspend fun create(title: String, description: String) {
 
-        val todoItem = TodoItem(title = title, description = description, isCompleted = false,)
+        val todoItem = TodoItem(title = title, description = description, isCompleted = false)
         withContext(Dispatchers.IO) {
             dao.create(todoItem)
         }
