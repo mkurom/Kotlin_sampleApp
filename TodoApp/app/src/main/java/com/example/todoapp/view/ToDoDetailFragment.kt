29985504100a7d821b2f340030f3724672e70f64
yasudamakoto <hare.v1v1enne@gmail.com>
2022-01-14 21:05:13 +0900

    fragmentにviewModelを追加

--- TodoApp/app/src/main/java/com/example/todoapp/view/ToDoDetailFragment.kt
+++ TodoApp/app/src/main/java/com/example/todoapp/view/ToDoDetailFragment.kt
@@ -1,7 +1,10 @@
 package com.example.todoapp.view
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.todoapp.R
+import com.example.todoapp.viewmodel.ToDoDetailViewModel
 
 class ToDoDetailFragment : Fragment(R.layout.todo_detail_fragment) {
+    private val vm: ToDoDetailViewModel by viewModels()
 }
