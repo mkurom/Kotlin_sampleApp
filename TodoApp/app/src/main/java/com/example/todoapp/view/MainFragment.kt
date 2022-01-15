a2266f109145b4d071a383ef91aba55874d88a4e
yasudamakoto <hare.v1v1enne@gmail.com>
2022-01-15 21:49:30 +0900

    Hilt導入

--- TodoApp/app/src/main/java/com/example/todoapp/view/MainFragment.kt
+++ TodoApp/app/src/main/java/com/example/todoapp/view/MainFragment.kt
@@ -4,7 +4,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.todoapp.R
 import com.example.todoapp.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainFragment : Fragment(R.layout.main_fragment) {
 
     private val vm: MainViewModel by viewModels()
