0555fb01f28d0b845078ce56e00e37badfa9fbf6
yasudamakoto <hare.v1v1enne@gmail.com>
2022-01-16 14:49:37 +0900

    ナビゲーションの修正

--- TodoApp/app/src/main/java/com/example/todoapp/view/MainFragment.kt
+++ TodoApp/app/src/main/java/com/example/todoapp/view/MainFragment.kt
@@ -1,8 +1,13 @@
 package com.example.todoapp.view
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.todoapp.R
+import com.example.todoapp.databinding.MainFragmentBinding
 import com.example.todoapp.viewmodel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,4 +15,21 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainFragment : Fragment(R.layout.main_fragment) {
 
     private val vm: MainViewModel by viewModels()
+
+    private var _binding: MainFragmentBinding? = null
+    private val binding: MainFragmentBinding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this._binding = MainFragmentBinding.bind(view)
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_createToDoFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        this._binding = null
+    }
 }
