dfd53e46c53c7227bff1efd46d4d3c0c14c1a095
yasudamakoto <hare.v1v1enne@gmail.com>
2022-01-19 01:31:50 +0900

    todoの詳細表示

--- TodoApp/app/src/main/java/com/example/todoapp/view/ToDoDetailFragment.kt
+++ TodoApp/app/src/main/java/com/example/todoapp/view/ToDoDetailFragment.kt
@@ -1,10 +1,38 @@
 package com.example.todoapp.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import com.example.todoapp.R
+import com.example.todoapp.databinding.TodoDetailFragmentBinding
 import com.example.todoapp.viewmodel.ToDoDetailViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ToDoDetailFragment : Fragment(R.layout.todo_detail_fragment) {
     private val vm: ToDoDetailViewModel by viewModels()
+
+    // nav_main.xmlでtoDoDetailFragment.xmlのargumentsに指定した引数
+    private val args: ToDoDetailFragmentArgs by navArgs()
+
+    private var _binding: TodoDetailFragmentBinding? = null
+    private val binding: TodoDetailFragmentBinding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        this._binding = TodoDetailFragmentBinding.bind(view)
+
+        // argumentsに追加した引数のName
+        val todoitem = args.todoitem
+
+        binding.titleText.text = todoitem.title
+        binding.detailText.text = todoitem.description
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        this._binding = null
+    }
 }
