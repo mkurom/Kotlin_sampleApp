9833cea263d7360a2e46b9433a2ecca00d976af2
yasudamakoto <hare.v1v1enne@gmail.com>
2022-02-05 03:15:13 +0900

    ボタンタップ時の処理を追加

--- CodeLabsApp/app/src/main/java/com/example/codelabsapp/MainActivity.kt
+++ CodeLabsApp/app/src/main/java/com/example/codelabsapp/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.codelabsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import android.widget.TextView
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCount = 0
+    private var mShowCount: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mShowCount = findViewById(R.id.show_count);
+    }
+
+    // xmlにandroid:onClick="showToast"を追加後、
+    // 「Create ‘showToast(view)' in MainActivity」で以下の関数を作成
+    fun showToast(view: View) {
+        // Toast ≒ スナックバー
+        val toast: Toast = Toast.makeText(this, R.string.toast_message,
+            Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    fun countUp(view: View) {
+        mCount++
+
+        mShowCount?.text = mCount.toString();
     }
 }
