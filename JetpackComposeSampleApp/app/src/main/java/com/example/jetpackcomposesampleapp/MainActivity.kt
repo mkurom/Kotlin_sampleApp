2a44dc6a5e9301001be93d6d29495141b56bfa2a
yasudamakoto <hare.v1v1enne@gmail.com>
2022-02-12 02:08:28 +0900

    edit Typography

--- JetpackComposeSampleApp/app/src/main/java/com/example/jetpackcomposesampleapp/MainActivity.kt
+++ JetpackComposeSampleApp/app/src/main/java/com/example/jetpackcomposesampleapp/MainActivity.kt
@@ -57,10 +57,15 @@ fun MessageCard(msg: Message) {
 
         Column {
             Text(text = msg.author,
-                color = MaterialTheme.colors.secondaryVariant
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
             )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Text(text = msg.body,
+                style = MaterialTheme.typography.body2
+            )
         }
     }
 }
