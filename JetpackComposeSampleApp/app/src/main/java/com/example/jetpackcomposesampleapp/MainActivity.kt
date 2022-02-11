c6e0fd6b7d2d70ad25099ab9d517aedd9fb98385
yasudamakoto <hare.v1v1enne@gmail.com>
2022-02-11 17:02:23 +0900

    Add an image element

--- JetpackComposeSampleApp/app/src/main/java/com/example/jetpackcomposesampleapp/MainActivity.kt
+++ JetpackComposeSampleApp/app/src/main/java/com/example/jetpackcomposesampleapp/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.jetpackcomposesampleapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.jetpackcomposesampleapp.ui.theme.JetpackComposeSampleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,15 +36,25 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row{
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
     }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
+    Image(
+        painter = painterResource(R.drawable.profile_picture),
+        contentDescription = "Contact profile picture",
+    )
     MessageCard(
         msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
     )
